fd874ada53ada5fe413cb461fefc9aa001d52ed2
fiadaze <fiakantapon@gmail.com>
2024-05-14 13:54:55 +0700

    fucntion

---
+++ function.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+
+const from = (firstName,location,hobby) => console.log(`Hi, my name is ${firstName} I live in  ${location}  and enjoy ${hobby}`);
+from("Kan","Nakhonphanom","Play video games")
